3782e50e9a402ae5e29d20eaf201b2249432b3dc
cematar <cematar@protem.com.tr>
2023-02-21 16:18:11 +0300

    changes2

--- CoreAPI/Controllers/ProductController.cs
+++ CoreAPI/Controllers/ProductController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace CoreAPI_Deneme2.Controllers
+namespace CoreAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
